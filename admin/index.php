26fd39f2291d12aaf08e11092716daef2a92baf6
Surjeet57 <surjeetmeena3003@gmail.com>
2021-12-24 13:40:38 +0530

    main

---
+++ admin/index.php
@@ -0,0 +1,37 @@
+<html>
+   <head>
+      <title>Railway pantry website-Home Page</title>
+   </head>
+
+   <body>
+      <!--Menu Section Starts-->
+      <div class="menu">
+         <div class="wrapper">
+         <ul>              
+               <li> <a href="#">Home</a></li>
+               <li><a href="#">Admin </a></li>
+               <li><a href="#">Category</a></li>
+               <li><a href="#">Order</a></li>
+            </ul> 
+         </div>          
+      </div>
+      <!--Menu section Ends-->
+
+      <!-- Main Content Section Starts-->
+      <div class="main-content">
+         <div class="wrapper">
+         Main Content Goes Here 
+         </div>
+      </div>
+      <!-- Main Content Section Ends-->
+
+      <!--Footer Section Starts-->
+      <div class="footer">
+         <div class="wrapper">
+               <p class='text-center'> 2021 All rights reserved, Developed by-<a herf="#">Surjeet Meena</a></p>
+         </div>
+      </div>
+      <!-- Footer Section Ends-->
+   </body>
+
+</html>
